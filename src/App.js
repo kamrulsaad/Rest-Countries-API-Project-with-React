62c5db722a759eef526926411d91b6fceab41af0
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-03-18 00:42:30 +0600

    Added image

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import Countries from './Countries/Countries';
 
 function App() {
 
-  const [countries,setCountries] = useState([]);
-  console.log(countries[0]);
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data));
-  },[])
   return (
     <div className="App">
-      {
-        countries.map(country => <Countries name ={country.name.common} continent={country.subregion} population = {country.population} capital = {country.capital}>
-        </Countries>)
-      }
+      <Countries></Countries>
     </div>
   );
-}
-
-function Countries(props){
-  return(
-    <div className='Card'>
-      <h3>{props.name}</h3>
-      <div className='Para-texts'>
-        <p>Region : {props.continent}</p>
-        <p>Population : {props.population} </p>
-        <p>Capital : {props.capital} </p>
-      </div>
-    </div>
-  )
 }
 
 export default App;
---
+++ src/Countries/Countries.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import Country from './Country';
+import './Countries.css'
+
+const Countries = () => {
+    const [countries,setCountries] = useState([]);
+    useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data));
+  },[])
+    return (
+        <div>
+            <h1>Search what you need!!</h1>
+            <div className="Countries">
+                {
+                    countries.map( country => <Country country = {country}></Country>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
